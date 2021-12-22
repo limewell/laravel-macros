fb70653f08366ffa46dacbc7bb46c8c92a18b617
Dipesh Sukhia <dipesh.sukhia@gmail.com>
2021-12-22 23:33:10 +0530

    hasmacro

--- Macros/EloquentBuilder/WithWhereHas.php
+++ Macros/EloquentBuilder/WithWhereHas.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Database\Eloquent\Builder;
 
-Builder::macro('withWhereHas', function (string $relation,Closure $callback = null): Builder {
-    return is_null($callback)
-        ? $this->whereHas($relation)->with([$relation])
-        : $this->whereHas($relation, $callback)->with([$relation => $callback]);
-});
+if (!Builder::hasGlobalMacro('withWhereHas')) {
+    Builder::macro('withWhereHas', function (string $relation, Closure $callback = null): Builder {
+        return is_null($callback)
+            ? $this->whereHas($relation)->with([$relation])
+            : $this->whereHas($relation, $callback)->with([$relation => $callback]);
+    });
+}
--- Macros/SchemaBlueprint/HasForeign.php
+++ Macros/SchemaBlueprint/HasForeign.php
@@ -4,17 +4,19 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-Blueprint::macro('hasForeign', function (string $table, string $foreignKey): bool {
-    $schema = DB::connection()->getDatabaseName();
-    return !empty(
-        DB::select(
-            DB::raw(
-                "SELECT * FROM information_schema.TABLE_CONSTRAINTS
-                WHERE information_schema.TABLE_CONSTRAINTS.CONSTRAINT_TYPE = 'FOREIGN KEY'
-                AND information_schema.TABLE_CONSTRAINTS.TABLE_SCHEMA = '{$schema}'
-                AND information_schema.TABLE_CONSTRAINTS.TABLE_NAME = '{$table}'
-                AND information_schema.TABLE_CONSTRAINTS.CONSTRAINT_NAME = '{$foreignKey}';"
+if (!Blueprint::hasMacro('withWhereHas')) {
+    Blueprint::macro('hasForeign', function (string $table, string $foreignKey): bool {
+        $schema = DB::connection()->getDatabaseName();
+        return !empty(
+            DB::select(
+                DB::raw(
+                    "SELECT * FROM information_schema.TABLE_CONSTRAINTS
+                        WHERE information_schema.TABLE_CONSTRAINTS.CONSTRAINT_TYPE = 'FOREIGN KEY'
+                        AND information_schema.TABLE_CONSTRAINTS.TABLE_SCHEMA = '{$schema}'
+                        AND information_schema.TABLE_CONSTRAINTS.TABLE_NAME = '{$table}'
+                        AND information_schema.TABLE_CONSTRAINTS.CONSTRAINT_NAME = '{$foreignKey}';"
+                )
             )
-        )
-    );
-});
+        );
+    });
+}
